67e7dfa9e3f44d5a33c537f02e0132ea73240f6c
ericpark11 <122037200+ericpark11@users.noreply.github.com>
2024-02-03 14:32:37 -0500

    wrwe4r

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 import MapView from 'react-native-maps';
+import React from 'react';
 
 
 export default function TabOneScreen() {
