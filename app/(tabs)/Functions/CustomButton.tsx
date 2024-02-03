520ea713bc09fa179fd7b10adfa3aa74f978ed05
Kenny Zhao <79938837+kennyzhao-code@users.noreply.github.com>
2024-02-03 14:24:39 -0500

    Add files via upload

---
+++ app/(tabs)/Functions/CustomButton.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, Button} from 'react-native';
+
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { Text, View } from '@/components/Themed';
+import styled from 'styled-components/native';
+
+type Props = {
+  text: string;
+}
+
+const CustomButton: React.FC<Props> = ({ text }: Props) => {
+  return (
+    <TouchableOpacity>
+        <View>
+            <Text>{text}</Text>
+        </View>
+    </TouchableOpacity>
+  )
+}
+const ButtonCont = styled.View`
+    flex: 1;
+`
+
+export default function TabTwoScreen() {
+  return (
+      <TouchableOpacity>
+        <Container>
+          <H1 >Report</H1>
+          <Text >y</Text>
+          <Row/>
+        </Container>
+      </TouchableOpacity>
+    
+  );
+}
+
+const Container = styled.View`
+    flex: 1;
+    align-Items: center;
+    justify-Content: center;
+    background-color: #5C677D;
+`
+
+const H1 = styled.Text`
+    font-Size: 20px;
+    color: rgba(218, 22, 22, 1);
+    font-Weight: bold;
+`
+
+const Row = styled.View`
+
+`
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,34 @@
+import { StyleSheet } from 'react-native';
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { Text, View } from '@/components/Themed';
+import MapView from 'react-native-maps';
+
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
